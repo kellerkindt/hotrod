a606792be0757c95eaeb954f2bcc988074c74c7c
Michael Watzko <michael@watzko.de>
2023-09-13 21:44:24 +0200

    Fix FpsManager not having consistent sleep times and not updating target fps

--- src/engine/system/vulkan/fps.rs
+++ src/engine/system/vulkan/fps.rs
@@ -1,7 +1,6 @@
 use std::time::{Duration, Instant};
 
 pub struct FpsManager {
-    target_frame_rate: u32,
     target_duration: Duration,
     last_instant: Option<Instant>,
 }
@@ -9,28 +8,27 @@ pub struct FpsManager {
 impl FpsManager {
     pub fn new(target_frame_rate: u32) -> Self {
         Self {
-            target_frame_rate,
             target_duration: Self::target_duration(target_frame_rate),
             last_instant: None,
         }
     }
 
     pub fn set_target_frame_rate(&mut self, target_frame_rate: u32) {
-        self.target_frame_rate = target_frame_rate;
+        self.target_duration = Self::target_duration(target_frame_rate);
     }
 
     pub fn delay(&mut self) -> Duration {
+        let mut slept = Duration::ZERO;
         if let Some(before) = self.last_instant.take() {
             let duration = before.elapsed();
             let target_duration = self.target_duration;
             if duration < target_duration {
-                let to_sleep = target_duration - duration;
-                std::thread::sleep(to_sleep);
-                return to_sleep;
+                slept = target_duration - duration;
+                std::thread::sleep(slept);
             }
         }
         self.last_instant = Some(Instant::now());
-        Duration::ZERO
+        slept
     }
 
     #[inline]
