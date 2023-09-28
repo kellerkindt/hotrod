cbf92d962cbe7802cc91a5dd30f5450ef4d5ec6c
Michael Watzko <michael@watzko.de>
2023-09-28 09:24:36 +0200

    Add world2::Rect<T>

--- src/engine/types/mod.rs
+++ src/engine/types/mod.rs
@@ -1,4 +1,17 @@
 pub mod world2d {
     pub type Pos<T> = cgmath::Point2<T>;
     pub type Dim<T> = cgmath::Vector2<T>;
+
+    #[derive(Debug, Copy, Clone, PartialEq)]
+    pub struct Rect<T> {
+        pub pos: Pos<T>,
+        pub dim: Dim<T>,
+    }
+
+    impl<T> Rect<T> {
+        #[inline]
+        pub const fn new(pos: Pos<T>, dim: Dim<T>) -> Self {
+            Self { pos, dim }
+        }
+    }
 }
