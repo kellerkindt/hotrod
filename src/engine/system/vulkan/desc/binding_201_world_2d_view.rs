a9859b30e022d1885f26bd756d059c636a90b9a1
Michael Watzko <michael@watzko.de>
2023-12-29 04:05:09 +0100

    Add zoom to World2dView

--- src/engine/system/vulkan/desc/binding_201_world_2d_view.rs
+++ src/engine/system/vulkan/desc/binding_201_world_2d_view.rs
@@ -4,25 +4,30 @@ use crate::engine::system::vulkan::system::VulkanSystem;
 pub struct World2dView {
     x: f32,
     y: f32,
+    zoom: f32,
 }
 
-impl From<[f32; 2]> for World2dView {
+impl From<[f32; 3]> for World2dView {
     #[inline]
-    fn from([x, y]: [f32; 2]) -> Self {
-        Self { x, y }
+    fn from([x, y, zoom]: [f32; 3]) -> Self {
+        Self { x, y, zoom }
     }
 }
 
 impl From<&VulkanSystem> for World2dView {
     #[inline]
     fn from(_vs: &VulkanSystem) -> Self {
-        Self { x: 0.0, y: 0.0 }
+        Self {
+            x: 0.0,
+            y: 0.0,
+            zoom: 1.0,
+        }
     }
 }
 
 impl WriteDescriptorSetOrigin for World2dView {
     type BufferContents = f32;
-    type Data = <[f32; 2] as IntoIterator>::IntoIter;
+    type Data = <[f32; 3] as IntoIterator>::IntoIter;
 
     #[inline]
     fn binding(&self) -> u32 {
@@ -31,6 +36,6 @@ impl WriteDescriptorSetOrigin for World2dView {
 
     #[inline]
     fn data(&self) -> Self::Data {
-        [self.x, self.y].into_iter()
+        [self.x, self.y, self.zoom].into_iter()
     }
 }
