58fc555877f9c00deb788bcb62228fb036fff486
Michael Watzko <michael@watzko.de>
2024-09-07 19:11:35 +0200

    Switch from log4rs to tracing-subscriber

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,8 @@ pub use sdl2;
 pub use vulkano;
 
 pub mod engine;
-pub mod logging;
 pub mod support;
 pub mod ui;
+
+#[cfg(feature = "logging-initializer")]
+pub mod logging;
--- src/logging.rs
+++ src/logging.rs
@@ -1,22 +1,19 @@
-use log4rs::append::console::ConsoleAppender;
-use log4rs::{Config};
-use log4rs::config::{Appender, Logger, Root};
-use log4rs::encode::pattern::PatternEncoder;
-pub use log::LevelFilter;
-pub use log::SetLoggerError;
+use std::error::Error;
+use tracing_subscriber::filter::LevelFilter;
+use tracing_subscriber::fmt::SubscriberBuilder;
 
-pub fn init_logger(level: Option<LevelFilter>) -> Result<::log4rs::Handle, SetLoggerError> {
-    let stdout = ConsoleAppender::builder()
-        .encoder(Box::new(PatternEncoder::new(
-            "{h({d(%Y-%m-%d %H:%M:%S%.3f)}  {M:>30.30}:{L:>03}  {T:>25.25}  {l:>5}  {m})}{n}",
-        )))
-        .build();
+#[inline]
+pub fn init_logger(level: Option<LevelFilter>) -> Result<(), Box<dyn Error + Send + Sync>> {
+    init_logger_with_customization(|builder| {
+        builder.with_max_level(level.unwrap_or_else(|| LevelFilter::WARN))
+    })
+}
 
-    let config = Config::builder()
-        .appender(Appender::builder().build("stdout", Box::new(stdout)))
-        .logger(Logger::builder().build(env!("CARGO_PKG_NAME"), level.unwrap_or(LevelFilter::Info)))
-        .build(Root::builder().appender("stdout").build(LevelFilter::Info))
-        .expect("Failed to create logger config");
-
-    ::log4rs::init_config(config)
+pub fn init_logger_with_customization(
+    f: impl FnOnce(SubscriberBuilder) -> SubscriberBuilder,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    f(tracing_subscriber::fmt()
+        .with_line_number(true)
+        .with_span_events(tracing_subscriber::fmt::format::FmtSpan::CLOSE))
+    .try_init()
 }
